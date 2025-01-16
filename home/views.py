543e74b402105b0d4da308a607f3736a844cf726
afy861345 <alialebady@gmail.com>
2025-01-16 08:33:50 +0300

    change1

--- home/views.py
+++ home/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("<h1>Hello Ali</h1>")
+def main(request):
+    return render(request,'home/main.html')
